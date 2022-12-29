6d793ef82f5562bf6c2f3b25671c83b2e71e4d52
ElenaPhoenix <sglena@mail.ru>
2022-12-29 08:41:14 +0300

    added string[] createNewAr in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿try
+﻿string[] CreateArray(int count) // Функция генерации массива указанной длины
+{
+    string[] array = new string[count];
+    Console.WriteLine($"Введите поочередно с новой строки {count} обособленных элементов");
+    for (int i = 0; i < count; i++) // Задаем поочередно с консоли элементы массива
+    {
+        array[i] = Console.ReadLine();
+    }
+    //for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]}"); // Выводим все элементы массива
+    return array;
+}
+
+
+try
 {
     Console.WriteLine($"Введите количество обособленных элементов, которые хотите ввести: ");
     int count = Convert.ToInt32(Console.ReadLine());
+
+    string[] createdNewAr = CreateArray(count);
 
 }
 catch
