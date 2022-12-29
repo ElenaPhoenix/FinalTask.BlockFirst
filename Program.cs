33ed9a87eb307795f1d336c5222841d7dbceb80c
ElenaPhoenix <sglena@mail.ru>
2022-12-29 08:38:33 +0300

    added BlockDiagram and input for user in Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿try
+{
+    Console.WriteLine($"Введите количество обособленных элементов, которые хотите ввести: ");
+    int count = Convert.ToInt32(Console.ReadLine());
+
+}
+catch
+{
+    Console.WriteLine("Ошибка. Попробуйте ввести число арабскими цифрами");
+}
